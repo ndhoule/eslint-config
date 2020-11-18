10bd02b883eec5aa8415b4449777f915f36b198e
Nathan Houle <nathan@nathanhoule.com>
2020-11-18 10:26:53 -0800

    feat: enable various TypeScript rules

--- recommended.js
+++ recommended.js
@@ -10,20 +10,23 @@ module.exports = {
     "plugin:prettier/recommended", // Must be last
   ],
   rules: {
+    // Force removal of unused eslint-disable directives, which can help prevent developers from
+    // reintroducing a bad pattern that was previously fixed but where the directive was left in
+    // place.
+    "eslint-comments/no-unused-disable": "error",
     "no-unused-vars": [
       "error",
       {
         // Ignore unused arguments that are prefixed with an underscore; naming unused variables is
         // sometimes helpful for forcing variadic functions to behave the way you want, or for
-        // documenting arguments that are likely to come into use in the future
+        // documenting arguments that are likely to come into use in the future.
         args: "after-used",
         argsIgnorePattern: "^_",
       },
     ],
-
-    // Force removal of unused eslint-disable directives, which can help prevent developers from
-    // reintroducing a bad pattern that was previously fixed but where the directive was left in
-    // place
-    "eslint-comments/no-unused-disable": "error",
+    "no-useless-constructor": "error",
+    // `return await` is almost always a mistake. The only time when it's acceptable is within a
+    // `try` block (which this rule permits).
+    "no-return-await": "error",
   },
 };
--- typescript.js
+++ typescript.js
@@ -4,19 +4,39 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:import/typescript",
 
     "prettier/@typescript-eslint", // Must be last
   ],
   rules: {
-    // Set to match recommended configuration
+    // Set to match `recommended` configuration
     "@typescript-eslint/no-unused-vars": [
       "error",
       { args: "after-used", argsIgnorePattern: "^_" },
     ],
-
     // Empty interfaces are often useful for module interfaces you expect to grow over time,
     // like program arguments, React props, etc.
     "@typescript-eslint/no-empty-interface": "off",
+    // Restrict `throw` to Error objects
+    "@typescript-eslint/no-throw-literal": "error",
+    // Prohibit empty class constructors
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": ["error"],
+    // Prohibit casting the initial value in Array#reduce calls (a very common antipattern).
+    "@typescript-eslint/prefer-reduce-type-parameter": "error",
+    // Require functions capable of returning a promise to be marked as `async`. Helps to improve
+    // stylistic consistency and to avoid functions that can return both a promise and a non-promise
+    // value (e.g. in a conditional).
+    "@typescript-eslint/promise-function-async": "error",
+    // Prohibit `return await` (see `recommended` configuration for details).
+    "no-return-await": "off",
+    "@typescript-eslint/return-await": "error",
+    // Disallow non-boolean values in boolean expressions (comparisons to a boolean, conditionals in
+    // ternaries/if/while/etc.)
+    "@typescript-eslint/strict-boolean-expressions": "error",
+
+    // "@typescript-eslint/": "error",
+    //no-use-before-define
   },
 };
